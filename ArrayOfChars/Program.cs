50addb57813459a9cced9cbe4c50993d248a85e3
Jörg Sundmacher <joerg.sundmacher@edicos.de>
2016-02-02 00:24:55 +0100

    Array-Beispiele

---
+++ ArrayOfChars/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayOfChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Mehrzeiliges Zeichenketten-Literal
+            string stringAsChars = "Dieser lange Satz sagt zwar nichts spezielles aus"+
+                " aber taugt aber als Beispiel zur Verdeutlichung.";
+            
+            // Initialisierung und Wertzuweisung mit HelperMethode
+            char[] charArray = stringAsChars.ToCharArray();
+            
+            // Umkehr der Zeichenfolge im ArrayOfChars
+            Array.Reverse(charArray);
+
+            // Iteration durch das Array
+            foreach (char stringChar in charArray)
+            {
+                Console.Write(stringChar);
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- ArrayShortNotation/Program.cs
+++ ArrayShortNotation/Program.cs
@@ -13,6 +13,12 @@ namespace ArrayShortNotation
             int[] numbers = new int[] { 0, 1, 2, 3, 4, 5, 6 };
             Console.WriteLine("Kurz Schreibweise: Array-Deklartation mit Wertzuweisung");
             Console.WriteLine("int[] numbers = new int[] { 0, 1, 2, 3, 4, 5, 6 };");
+            Console.ReadLine();
+
+            // Bestimmen der Länge/Anzahl von Einträgen eines Arrays mittels Length-Property
+            Console.WriteLine("Ausgabe der Länge des Arrays mittels Legth-Property");
+            Console.WriteLine("numbers.Length  => "+numbers.Length);
+            Console.ReadLine();
         }
     }
 }

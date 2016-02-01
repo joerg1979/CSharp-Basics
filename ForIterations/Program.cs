4a80aeb62bc87483b389b023dbffbc878bc83d09
Jörg Sundmacher <joerg.sundmacher@edicos.de>
2016-02-01 15:05:48 +0100

    For-Schleifen unterbrechen mit "break"

--- ForIterations/Program.cs
+++ ForIterations/Program.cs
@@ -13,16 +13,38 @@ namespace ForIterations
             // Array of Strings zum durhclaufen mit der for-Schleife
             String[] forIterationsText =
             {
-                "Syntax einer einfachen for-Schleife...\n",
+                "Syntax einer einfachen for-Schleife:",
                 "for(int i = 0; i < 4; i++){",
                 "   Auszuführender Code-Block;",
                 "}",
             };
+            Console.WriteLine(forIterationsText[0]);
             // Verarbeitung des Arrays in der forSchleife
             for (int i = 1; i < forIterationsText.Length ; i++)
             {
                 Console.Write("Durchlauf " + i.ToString()+": ");
                 Console.Write(forIterationsText[i]+"\n");
+            }
+            Console.ReadLine();
+            Console.WriteLine("...beeinflussen von Schleifendurchläufen:");
+            Console.ReadLine();
+            // Unterbrechen von Schleifendurchläufen
+            for (int i = 0; i < 5; i++)
+            {
+                if (i == 2)
+                {
+                    Console.WriteLine("Der Durchlauf von For-Schleifen kann abgebrochen werden ");                    
+                }
+                if (i == 3)
+                {
+                    Console.WriteLine("Zum Abbruch kann dass \"break;\"-Statement verwendet werden.");
+                    for(int j =1;j<=2;j++)
+                    {
+                        Console.WriteLine(forIterationsText[j]);
+                    }
+                    Console.WriteLine("   break;\n}");
+                    break;
+                }
             }
             Console.ReadLine();
         }

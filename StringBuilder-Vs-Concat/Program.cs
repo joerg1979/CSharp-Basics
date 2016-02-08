4908fcfe76ddaaab346d9aac82182d1dcf1dfe2e
Jörg <js_edv@yahoo.de>
2016-02-08 18:45:47 +0100

    .cs Files added

---
+++ StringBuilder-Vs-Concat/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuilder_Vs_Concat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string concatInfoText = "Zeichenketten können mittels +(Concat-Operator) verbunden werden. \nAllerdings führt dies dazu dass für jede Verkettung ein neuer " +
+                "Speicherbereich für das neue String-Objekts reserviert und zugewiesen werden muss. \nIm kleinen Rahmen ist dies nicht problematisch, allerdings sollte " +
+                "besser der StringBuilder() verwendet werden. \nDiese Klasse ist genau für diesen Anwendugsfall konzipiert und umgeht die Speicherverschwendung";
+
+            Console.WriteLine(concatInfoText);
+            Console.ReadLine();
+
+            string concatString = "0";
+            for (int i = 1; i < 100; i++)
+            {
+                concatString += "--" + i.ToString();
+                if (i % 25 == 0)
+                    concatString += "\n";
+            }
+            Console.WriteLine(concatString);
+            Console.WriteLine("100 Stringverkettungen (--i)\nVerwendeter Befehl innerhalb des Schleifenrumpfs: { myString += \"--\" + i.ToString();}");
+            Console.ReadLine();
+
+            Console.WriteLine("Das selbe Ergebnis mit StringBuilder() => Initialisieren mit: StringBuilder sb = new StringBuilder(); ");
+            StringBuilder sb = new StringBuilder();
+            sb.Append("0");
+            for (int i = 1; i < 100; i++)
+            {
+                sb.Append("--").Append(i);
+                if (i % 25 == 0)
+                    sb.Append("\n");
+            }
+            Console.WriteLine(sb.ToString());
+            Console.WriteLine("Verwendeter Befehl innerhalb des Schleifenrumpfs: {sb.Append(\"--\").Append(i);}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StringManipulationMethods/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulationMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringToGetSubstringed = "Ein bestimmter Teil einer Zeichenkette kann mittels Substring(von,bis) erzeugt werden.\n" +
+                "Beispiel: stringToGetSubstringed.Substring(0, 83) => liefert die ersten 84 Zeichen der vorherigen Zeichenkette!";
+            Console.WriteLine(stringToGetSubstringed);
+            Console.ReadLine();
+            Console.WriteLine(stringToGetSubstringed.Substring(0, 83));
+            Console.ReadLine();
+
+            Console.WriteLine("Eine Zeichenkette in Großbuchstaben umwandeln mittels:  lowerCaseString.ToUpper() ");
+            string lowerCaseString = "alles in kleinen buchstaben";
+            Console.WriteLine(lowerCaseString + " => " + lowerCaseString.ToUpper());
+            Console.ReadLine();
+
+            Console.WriteLine("Alle \"Leerzeichen\" einer Zeichenkette umwandeln zu \"--\" mittels: lowerCaseString.Replace(\"\",\"--\")");
+            Console.WriteLine(lowerCaseString + " => " + lowerCaseString.Replace(" ", "--"));
+            Console.ReadLine();
+
+            string string2Trim = "   Die 3 Leerzeichen am Beginn und am Ende dieser Zeichenkette können entfernt mittels \"string2Trim.Trim()\" entfernt werden.   ";
+            Console.WriteLine(string2Trim);
+            Console.ReadLine();
+            Console.WriteLine("Länge vor Trim(): {0} => Länge nach Trim(): {1}", string2Trim.Length, string2Trim.Trim().Length);
+            Console.WriteLine("Ausgeführter Code: Console.WriteLine(\"Länge vor Trim(): {0} => Länge nach Trim(): {1}\", string2Trim.Length, string2Trim.Trim().Length\");");
+            Console.ReadLine();
+        }
+    }
+}

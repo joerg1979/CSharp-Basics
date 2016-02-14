ae8294783604af73d220f59733f1a97586526855
Jörg <js_edv@yahoo.de>
2016-02-14 19:35:33 +0100

    added Inheritance-Example

---
+++ Inheritance/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static void printMe(Vehicle vehicle)
+        {
+            Console.WriteLine("");
+        }
+
+    }
+    class Vehicle
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Year { get; set; }
+
+    }
+    class Car : Vehicle
+    {
+        public string Doors { get; set; }
+    }
+
+    class Truck : Vehicle
+    {
+        public int TowingCapacity { get; set; }
+
+    }
+}
